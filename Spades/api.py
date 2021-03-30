8c6c2531d025584260172c24496a52a03d88e74e
Lucas <teppss@gmail.com>
2021-03-30 22:29:28 +0100

    public POST

--- Spades/api.py
+++ Spades/api.py
@@ -2,13 +2,19 @@ from . import models
 from . import serializers
 from rest_framework import viewsets, permissions
 
+class CanPost(permissions.BasePermission):
+    def has_permission(self, request, view):
+        return request.method in ['POST']
 
 class EventViewSet(viewsets.ModelViewSet):
     """ViewSet for the Event class"""
 
     queryset = models.Event.objects.all()
     serializer_class = serializers.EventSerializer
-    permission_classes = [permissions.IsAdminUser]
+    permission_classes = [permissions.IsAdminUser|CanPost]
 
     def perform_create(self, serializer):
-        serializer.save(user=self.request.user)
+        if self.request.user.id:
+            serializer.save(user=self.request.user)
+        else:
+            serializer.save()
