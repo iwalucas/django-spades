0c6532201de8b3cfa630ee4fafa6eeda2e602542
Lucas Costa <lucas@thesmartpanda.com>
2025-02-05 16:47:09 +0000

    Update Event model to use JSONField for info and adjust migrations

--- Spades/models.py
+++ Spades/models.py
@@ -1,59 +1,26 @@
 from django.urls import reverse
-from django.db.models import CharField
-from django.db.models import DateTimeField
-from django.db.models import TextField
 from django.conf import settings
-from django.contrib.contenttypes.fields import GenericForeignKey
-from django.contrib.contenttypes.models import ContentType
-from django.contrib.auth import get_user_model
-from django.contrib.auth import models as auth_models
-from django.db import models as models 
-import json
-from django.contrib.auth.models import User
-from django.core.exceptions import ValidationError
+from django.db import models as models
 from django.conf import settings
-
 
 
 class Event(models.Model):
-    # Fields
     name = models.CharField(max_length=255)
     created = models.DateTimeField(auto_now_add=True, editable=False)
     last_updated = models.DateTimeField(auto_now=True, editable=False)
-    info = models.TextField()
+    info = models.JSONField()
     user = models.ForeignKey(
-            settings.AUTH_USER_MODEL,
-            on_delete=models.CASCADE,
-            blank=True,null=True
-        )
+        settings.AUTH_USER_MODEL, on_delete=models.CASCADE, blank=True, null=True
+    )
 
     class Meta:
-        ordering = ('-created',)
+        ordering = ("-created",)
 
     def __str__(self):
-        return u'%s' % self.pk
+        return "%s" % self.pk
 
     def get_absolute_url(self):
-        return reverse('EventTracker_event_detail', args=(self.pk,))
-
+        return reverse("Spades_event_detail", args=(self.pk,))
 
     def get_update_url(self):
-        return reverse('EventTracker_event_update', args=(self.pk,))
-
-    # def user(self):
-    #     j = json.loads(self.info)
-    #     if 'User' in j:
-    #         user = User.objects.get(id=j['User'])
-    #         return user
-
-    def save(self, *args, **kwargs):
-        try:
-            j = json.loads(self.info)
-        except:
-            raise ValidationError('Info needs to be of JSON format')
-
-
-        super().save(*args, **kwargs)  
-
-
-
+        return reverse("Spades_event_update", args=(self.pk,))
--- setup.py
+++ setup.py
@@ -1,35 +1,34 @@
 import os
 from setuptools import find_packages, setup
 
-with open(os.path.join(os.path.dirname(__file__), 'README.rst')) as readme:
+with open(os.path.join(os.path.dirname(__file__), "README.rst")) as readme:
     README = readme.read()
 
 # allow setup.py to be run from any path
 os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
-    name='django-spades',
-    version='0.1.9',
+    name="django-spades",
+    version="0.1.10",
     packages=find_packages(),
     include_package_data=True,
-    license='MIT License',
-    description='Django app to track generic events and keep records of unstructured data.',
+    license="MIT License",
+    description="Django app to track generic events and keep records of unstructured data.",
     long_description=README,
-    url='https://github.com/iwalucas/django-spades/',
-    author='Lucas',
-    author_email='teppss@gmail.com',
+    url="https://github.com/iwalucas/django-spades/",
+    author="Lucas",
+    author_email="teppss@gmail.com",
     classifiers=[
-        'Environment :: Web Environment',
-        'Framework :: Django',
-        'Framework :: Django :: 2.2',
-        'Framework :: Django :: 3.0',
-        'Intended Audience :: Developers',
-        'License :: OSI Approved :: MIT License',
-        'Operating System :: OS Independent',
-        'Programming Language :: Python',
-        'Programming Language :: Python :: 3.5',
-        'Programming Language :: Python :: 3.6',
-        'Topic :: Internet :: WWW/HTTP',
-        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+        "Environment :: Web Environment",
+        "Framework :: Django",
+        "Framework :: Django :: 3.2",
+        "Framework :: Django :: 5.1",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3.11",
+        "Topic :: Internet :: WWW/HTTP",
+        "Topic :: Internet :: WWW/HTTP :: Dynamic Content",
     ],
 )
